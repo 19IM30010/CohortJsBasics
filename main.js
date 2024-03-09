74b5f01122eac35dcc4a67893d2e477a7d14a1d8
19IM30010 <gyanendrajsp2001@gmail.com>
2024-03-09 20:00:24 +0530

    Till Call back functions

---
+++ main.js
@@ -0,0 +1,68 @@
+// Basics
+console.log(
+  "Hello World, this is [Fatima] [Abu] with HNGi7 ID [HNG"
+);
+// console.log(a);
+let firstName="Yo"
+let lastName="Abu"
+console.log("Hi" + " " + firstName + " " + lastName)
+// console.log(b);
+var a=1;
+a=2;
+console.log(a);
+var tr=true;
+if(tr){
+  console.log(true);
+}
+else{
+  console.log(false);
+}
+// var sum = 0;
+// for(var i=0;i<=10;i++){
+//   console.log(sum+=i);
+// }
+
+
+// Arrays and Objects //
+
+var arr=[1,2,3,4,5,6,7,8,9,10];
+for(var i=0;i<arr.length;i++){
+  if(arr[i]%2==0){
+    console.log(arr[i]);
+  }
+}
+const user1 = {
+  firstName: "Fatima",
+  lastName: "Abu",
+  id: "HNG-01484",
+  email: "tugrp@example.com",
+  language: "Javascript",
+}
+const allUsers= [
+  {firstName:"Gyan",gender:"Male"},
+  {firstName:"Abu",gender:"Male"},
+  {firstName:"Harsa",gender:"Female"}
+];
+
+for(var i=0;i<allUsers.length;i++){
+  if(allUsers[i]['gender']=='Male'){
+    console.log(allUsers[i].firstName);
+  }
+}
+
+// Function // call Back Function
+function sum(a,b,fnToCall){
+    let result = a+b;
+    fnToCall(result);
+    
+  }
+  function displayResult(data){
+    console.log("Result of the sum is: " + data);
+  }
+  function displayResultPassive(data){
+    console.log("Sum's Result is: " + data);
+  }
+  
+  console.log(sum(3,4,displayResult));
+  console.log(sum(3,4,displayResultPassive));
+  
